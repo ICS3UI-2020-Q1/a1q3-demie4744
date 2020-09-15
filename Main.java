ca4810691106e36c8b5a6b7871ec21e796ec1ec3
demie4744 <demie4744@wrdsb.ca>
2020-09-15 16:14:44 +0000

    I made it say the prices

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * How many different qualitys of computers can she get
+ * @author Erkam
  */
 public class Main {
 
@@ -9,7 +10,30 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // //// Creates a Scanner used for input
+   Scanner input = new Scanner(System.in);
     
+    System.out.println("How many chromebook chargers would you like to purchase?");
+     int Chromebook  = input.nextInt();
+
+     System.out.println("that will be 118.65$ so far");
+
+     System.out.println("How many mice?");
+     int mice = input.nextInt();
+
+     System.out.println("220.35");
+
+     System.out.println("How many motherboards?");
+     int motherboards = input.nextInt();
+
+  
+
+     System.out.println("Total with Tax included = $508.46");
+
+
+
+
+
+
   }
 }
